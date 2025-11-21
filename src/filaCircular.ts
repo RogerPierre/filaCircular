0c8cee68a9e0a6b5bf1cc149399583cc31fbce9c
ROGER PIERRE REIS SILVA <2025116TADS0010@ifpi.int>
2025-11-21 15:11:11 -0300

    Add methods for Filacircular.

--- src/filaCircular.ts
+++ src/filaCircular.ts
@@ -1,17 +1,47 @@
 export class filaCircular {
     capacidade: number; 
     fila: number[];
-    frente: number
-    tras: number;
+    frente: number|null
+    tras: number|null;
     tamanho: number;
-
+    //*/ Método construtor para inicializar a fila circular
     constructor(capacidade: number) {
         this.capacidade = capacidade;
         this.fila = [];
-        this.frente = 0;
-        this.tras = -1;
+        this.frente = null;
+        this.tras = null;
         this.tamanho = 0;
     }
-    
+    verificarSeCheia(): boolean {
+        return this.tamanho === this.capacidade;
+    }
+    verificarSeVazia(): boolean {
+        return this.tamanho === 0;
+    }
+    modificarTamanho():void{
+        this.tamanho=this.fila.length;
+    }
+    adicionarElemento(elemento: number): void {
+        if (this.verificarSeCheia()) {
+            console.log("A fila está cheia. Não é possível adicionar mais elementos.");
+        }else{
+            this.fila.push(elemento);
+            this.frente=this.fila[this.tamanho-1];
+            this.modificarTamanho()
+            
+        }
+    }
+    removerElemento(): number | null {
+        if (this.verificarSeVazia()) {
+            console.log("A fila está vazia. Não há elementos para remover.");
+            return null;
+        }else{
+            let elementoDeTras=this.tras;
+
+            return elementoDeTras;
+            
+        }
+    }
+
 
 }
