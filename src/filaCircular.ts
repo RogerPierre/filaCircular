ce466e9a6464c31f1173a96928313138696aa12b
ROGER PIERRE REIS SILVA <2025116TADS0010@ifpi.int>
2025-11-21 15:58:09 -0300

    adição de methodos e uma arquivo ui.ts

--- src/filaCircular.ts
+++ src/filaCircular.ts
@@ -12,35 +12,69 @@ export class filaCircular {
         this.tras = null;
         this.tamanho = 0;
     }
+
+
+
+
+// Exibir quantos elementos ainda podem ser inseridos na fila.
+
+
+// Verificar se a fila está cheia.
     verificarSeCheia(): boolean {
         return this.tamanho === this.capacidade;
     }
+    // Verificar se a fila está vazia.
     verificarSeVazia(): boolean {
         return this.tamanho === 0;
     }
     modificarTamanho():void{
         this.tamanho=this.fila.length;
     }
-    primeiroElemento():number|null{
+// Exibir a quantidade de elementos da fila.
+    mostrarTamanho():number{
+        return this.tamanho;
+    }
+    // Retornar a capacidade máxima da fila.
+    mostrarCapacidade():number{
+        return this.capacidade
+    }
+    // Retornar o elemento que está no início da fila .
+    RetornarPrimeiroElemento():number|null{
         if(this.verificarSeVazia()){
             return null;
         }
         return this.fila[0];
     }
-    adicionarElemento(elemento: number): void {
+// Exibir os elementos da fila.
+    exibirFila(){
+        return this.fila;
+    }
+    // Retornar o elemento que está no início da fila .
+    retornarElementoInicial():number|string{
+        if(this.verificarSeVazia()){
+            return "Nao e possivel adicionar elementos porque esta lista está vazia."
+        }else{
+            return this.fila[this.tamanho-1];
+        }
+    }
+   
+    //     Adicionar um novo elemento ao final da fila.
+    adicionarElemento(elemento: number): string | (number|null) {
         if (this.verificarSeCheia()) {
-            console.log("A fila está cheia. Não é possível adicionar mais elementos.");
+            return "Cheia.";
         }else{
             this.fila.push(elemento);
             this.frente=this.fila[this.tamanho-1];
             this.modificarTamanho();
+            return "Adicionada."
             
         }
     }
-    removerElemento(): number | null {
+    
+    // Remover e retornar o primeiro elemento da fila (o elemento mais antigo).
+    removerElemento():string | ( number | null) {
         if (this.verificarSeVazia()) {
-            console.log("A fila está vazia. Não há elementos para remover.");
-            return null;
+            return "A fila está vazia. Não há elementos para remover.";
         }else{
             let elementoDeTras=this.tras;
             this.fila.shift();
@@ -48,6 +82,9 @@ export class filaCircular {
             this.tras=this.fila[this.tamanho-1];
             return elementoDeTras;
         }
+    }
+    retornarQuantosEspacosFaltam(){
+        return this.capacidade-this.tamanho;
     }
 
 
